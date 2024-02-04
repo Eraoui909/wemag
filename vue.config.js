5df5f8392eceb891a93fb34bff8e64da9d46e171
Hamza Eraoui <hamzaprofessionnel1234@gmail.com>
2024-02-04 21:53:13 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
+    /*publicPath: process.env.NODE_ENV === 'production'
         ? '/'
-        : '',
+        : '',*/
+    publicPath: '/wemag/',
     //Other options include:
     outputDir: undefined,
     assetsDir: undefined,
