ead9d1bb8ca5df467390c97592c505df4d71f40a
hamza eraoui <hamza.eraoui@oracle.com>
2025-06-03 23:43:57 +0100

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    /*publicPath: process.env.NODE_ENV === 'production'
+    publicPath: process.env.NODE_ENV === 'production'
         ? '/'
-        : '',*/
-    publicPath: '/wemag/',
+        : '',
     //Other options include:
     outputDir: undefined,
     assetsDir: undefined,
